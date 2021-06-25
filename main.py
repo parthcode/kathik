0e8bc35d163df1f14efbb9882370d18f9a77d43e
parthamohanty <parthsit@gmail.com>
2021-06-25 09:42:11 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+calculator :
+1.two input parameter
+2.it can ask user to input the parameter
+3.add , substract , multiply and show the relevant results
+"""
+first = input("Enter the first number")
+second = input("Enter the second number")
+print(f'Sum is :{int(first) + int(second)}')
